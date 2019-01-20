0ecea6d9ab656b7810793257e6910d529eaf6a21
rehellinen <912377791@qq.com>
2019-01-20 10:47:20 +0800

    base environment building

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ *  index.js
+ *  Create By rehellinen
+ *  Create On 2018/11/28 10:24
+ */
+class Run {
+  constructor () {
+    this.app = document.getElementById('app')
+  }
+
+  start () {
+    this.app.innerHTML="<p>test</p>"
+  }
+}
+
+new Run().start()
