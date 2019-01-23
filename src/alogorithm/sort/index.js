9c873142d937d33305c5a9e9321113ce8d2c6ef3
rehellinen <912377791@qq.com>
2019-01-23 22:00:10 +0800

    测试方法改进

--- src/alogorithm/sort/index.js
+++ src/alogorithm/sort/index.js
@@ -16,16 +16,20 @@ import {generateNearlyOrderedArr, generateRandomArr, performanceTesting} from ".
 
 const test = (func, length, testTimes = 10) => {
   let random = 0
+  let limited = 0
   let ordered = 0
   for (let i = 0; i < testTimes; i++) {
-    const randomTime = performanceTesting(func, generateRandomArr({length, max: 15000}))
+    const randomTime = performanceTesting(func, generateRandomArr({length, max: length}))
+    const limitedTime = performanceTesting(func, generateRandomArr({length, max: 3}))
     const orderedTime = performanceTesting(func, generateNearlyOrderedArr({length, swapTimes: 100}))
 
     random += randomTime
     ordered += orderedTime
+    limited += limitedTime
   }
   console.log(chalk.blue(`function name: ${func.name}`))
   console.log(chalk.cyan(`random: ${random / testTimes}ms used!`))
+  console.log(chalk.cyan(`limited range: ${limited / testTimes}ms used!`))
   console.log(chalk.cyan(`nearly ordered: ${ordered / testTimes}ms used!`))
   console.log('')
 }
