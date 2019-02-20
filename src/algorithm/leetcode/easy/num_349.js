01f5ce4eb8e67eb364cef64cd22f9b13f6b4a8f0
rehellinen <912377791@qq.com>
2019-02-20 20:18:38 +0800

    leetcode - no.349

--- src/algorithm/leetcode/easy/num_349.js
+++ src/algorithm/leetcode/easy/num_349.js
@@ -1,3 +1,13 @@
 const intersection = (arr1, arr2) => {
+  const set1 = new Set(arr1)
 
+  const res = new Set()
+  for (let num of arr2) {
+    if (set1.has(num)) {
+      res.add(num)
+    }
+  }
+  return [...res]
 }
+
+intersection([1,2,2,1], [2,2])
