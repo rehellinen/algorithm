065d421928c0a47b9c8a529cc3d998ee74d382ef
rehellinen <912377791@qq.com>
2019-04-02 16:01:19 +0800

    segment tree - edit

--- src/data_structure/tree/segment/SegmentTree.js
+++ src/data_structure/tree/segment/SegmentTree.js
@@ -3,8 +3,13 @@
  *  更新一个元素或一个区间的值效率更高
  *  还能进行统计查询（如查询一个区间的最大值、最小值、总和等等）
  *  用数组存储需要4*n的空间
+ *  更新、查询 - O(logn)
+ *  建立 - O(n)
  */
 
+// TODO: 区间更新、懒惰更新
+// TODO: 二维线段树（思想：大拆分成小）
+// TODO: 动态（创建）线段树
 class SegmentTree {
   tree = []
   data = []
@@ -41,6 +46,31 @@ class SegmentTree {
 
     const leftRes = this._query(leftIndex, left, mid, queryL, mid)
     const rightRes = this._query(right, mid+1, right, queryL, mid)
+  }
+
+  // 将i位置的值改为val
+  set (index, value) {
+    if (index < 0 || index >= this.data.length) return 'error index'
+    data[index] = value
+    this._set(0, 0, this.data.length-1, index, value)
+  }
+  _set (treeIndex, left, right, index, value) {
+    if (left === right) {
+      this.tree[treeIndex] = value
+      return
+    }
+
+    const leftIndex = this.leftChild(treeIndex)
+    const rightIndex = this.rightChild(treeIndex)
+    const mid = Math.floor(left + (left - right) / 2)
+
+    if (index >= mid + 1) {
+      this._set(rightIndex, mid+1, right, index, value)
+    } else {
+      this._set(leftIndex, left, mid, index, value)
+    }
+
+    this.tree[treeIndex] = this.tree[leftIndex] + this.tree[rightIndex]
   }
 
   // 建立线段树
